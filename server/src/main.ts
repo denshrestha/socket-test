2d535ecd4c416bed4ef94316f3ad35d8ca3cf2d1
d.shrestha <denis.shrestha@gmail.com>
2023-04-01 16:42:33 +0300

    package.json update

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Main (main.ts)');
+
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: [
+        'http://localhost:8080',
+        /^http:\/\/192\.168\.1\.([1-9]|[1-9]\d):8080$/,
+      ],
+    },
+  });
   await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+  const port = parseInt(configService.get('PORT'));
+  await app.listen(port);
+
+  logger.log(`Server running on port ${port}`);
 }
 bootstrap();
